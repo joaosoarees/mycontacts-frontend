2b6e65791c52293cfafa3ff7ed7087a6a6d2abe1
João Vitor <jsoareez@gmail.com>
2021-07-28 23:38:19 -0300

    Input & Select Component

---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  background: ${({ theme }) => theme.colors.primary.background};
+  border: 2px solid ${({ theme }) => theme.colors.primary.background};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  border-radius: 4px;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,17 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
-  return <PageHeader title="Novo Contato" />;
+  return (
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <Input placeholder="Nome" />
+
+      <Select>
+        <option value="123">instagram</option>
+      </Select>
+    </>
+  );
 }
