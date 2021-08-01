3d4568c2e4795faf18f7769489bed1e400829fd0
João Vitor <jsoareez@gmail.com>
2021-08-01 18:58:46 -0300

    mobile adjust

--- src/components/Input.js
+++ src/components/Input.js
@@ -11,6 +11,7 @@ export default styled.input`
   padding: 0 16px;
   font-size: 16px;
   transition: border-color 0.2s ease-in;
+  appearance: none;
 
   &:focus {
     border-color: ${({ theme }) => theme.colors.primary.main};
