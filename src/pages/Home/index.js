1e1e3dff343397bf72c8552c6fbe72d6a77a762d
João Vitor <jsoareez@gmail.com>
2021-12-24 20:09:30 -0300

    input search handler container

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,7 +11,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
-// import magnifierQuestion from '../../assets/images/magnifier-question.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -127,6 +127,24 @@ export default function Home() {
               </p>
             </S.EmptyListContainer>
           )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+            <S.SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Lupa não encontrado." />
+
+              <span>
+                Nenhum resultado foi encontrado para
+                {' '}
+                <strong>
+                  ”
+                  {searchTerm}
+                  ”
+                </strong>
+                .
+              </span>
+            </S.SearchNotFoundContainer>
+          )}
+
           {filteredContacts.length > 0 && (
           <S.ListHeader orderBy={orderBy}>
             <button type="button" onClick={handleToggleOrderBy}>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -156,3 +156,15 @@ export const EmptyListContainer = styled.div`
     }
   }
 `;
+
+export const SearchNotFoundContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  align-items: flex-start;
+
+  span {
+    color: ${({ theme }) => theme.colors.gray[200]};
+    margin-left: 24px;
+    word-break: break-word;
+  }
+`;
