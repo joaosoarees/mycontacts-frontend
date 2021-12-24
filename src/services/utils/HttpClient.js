6561659a445337c91a396f78187f7c342bc7d442
João Vitor <jsoareez@gmail.com>
2021-12-24 01:22:37 -0300

    start to treat errors

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -10,11 +10,21 @@ class HttpClient {
 
     const response = await fetch(`${this.baseUrl}${path}`);
 
-    if (response.ok) {
-      return response.json();
+    const contentType = response.headers.get('Content-Type');
+
+    let body = null;
+
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 
