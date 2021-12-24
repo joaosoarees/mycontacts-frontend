10148f6636a53edc538a4a9ecf0040e96aa1de7c
João Vitor <jsoareez@gmail.com>
2021-12-24 00:42:54 -0300

    throwing errors

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -29,7 +29,7 @@ export default function Home() {
 
         setContacts(contactsList);
       } catch (error) {
-        console.log('error', error);
+        console.log(error);
       } finally {
         setIsLoading(false);
       }
--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -6,11 +6,15 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseUrl}${path}`);
-
     await delay(500);
 
-    return response.json();
+    const response = await fetch(`${this.baseUrl}${path}`);
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status} - ${response.statusText}`);
   }
 }
 
