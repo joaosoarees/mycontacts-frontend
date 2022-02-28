dbcf9e34ae470106b6188233837985640b524981
João Vitor <jsoareez@gmail.com>
2022-02-28 15:31:10 -0300

    feat: add no-caught try catch errors

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -31,9 +31,11 @@ export default function ContactForm({ buttonLabel }) {
 
   useEffect(() => {
     async function loadCategories() {
-      const categoriesList = await CategoriesService.listCategories();
+      try {
+        const categoriesList = await CategoriesService.listCategories();
 
-      setCategories(categoriesList);
+        setCategories(categoriesList);
+      } catch {}
     }
 
     loadCategories();
