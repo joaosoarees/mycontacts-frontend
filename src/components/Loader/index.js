4fb9a8f4364e052451b92c96cf7c3a6f61373917
João Vitor <jsoareez@gmail.com>
2022-02-28 16:08:17 -0300

    feat: Spinner Component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,13 +2,14 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import * as S from './styles';
+import Spinner from '../Spinner';
 
 export default function Loader({ isLoading }) {
   if (!isLoading) return null;
 
   return ReactDOM.createPortal(
     <S.Overlay>
-      <div className="loader" />
+      <Spinner size={90} />
     </S.Overlay>,
     document.getElementById('loader-root'),
   );
